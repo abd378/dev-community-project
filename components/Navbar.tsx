fe7018460ab83d171d611df4e8c727124a40aeed
abdosman81234 <oabed5527@gmail.com>
2026-05-15 14:20:39 +0300

    move navbar to bottom

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,39 +3,13 @@ import NotificationBadge from "@/components/NotificationBadge";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-left-section">
-        <details className="menu-details">
-          <summary className="three-dots-btn">⋮</summary>
-
-          <div className="side-dropdown-menu">
-            <Link href="/">Home</Link>
-            <Link href="/communities">Communities</Link>
-            <Link href="/topics">Topics</Link>
-            <Link href="/developers">Developers</Link>
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/posts">Posts</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/chat">Chat</Link>
-          </div>
-        </details>
-      </div>
-
-      <Link href="/" className="logo">
-        DEVHUB
-      </Link>
-
-      <details className="profile-menu">
-        <summary className="profile-icon">👤</summary>
-
-        <div className="profile-dropdown">
-          <Link href="/stories">Stories</Link>
-          <NotificationBadge />
-          <Link href="/profile">Profile</Link>
-          <Link href="/login">Login</Link>
-          <Link href="/signup">Signup</Link>
-        </div>
-      </details>
+    <nav className="bottom-navbar">
+      <Link href="/" className="bottom-nav-item">🏠</Link>
+      <Link href="/developers" className="bottom-nav-item">👥</Link>
+      <Link href="/posts" className="bottom-nav-item">➕</Link>
+      <Link href="/chat" className="bottom-nav-item">💬</Link>
+      <NotificationBadge />
+      <Link href="/profile" className="bottom-nav-item">👤</Link>
     </nav>
   );
 }
